f4d09eb2c6af75bdbec026ff86f38c67f68d6d8c
samibi6 <sambiname@yahoo.fr>
2024-01-07 12:11:59 +0100

    title color

--- pages/index.vue
+++ pages/index.vue
@@ -93,33 +93,37 @@ const pokemonsFiltered = computed(() => {
 
 <template>
   <div class="flex">
-    <div class="bg-red-500 w-1/2 p-5 rounded-l-lg">
-      <div class="py-5 px-3 space-x-4 bg-zinc-600 rounded-t-lg shadow-xl">
-        <input type="text" v-model="searchTxt" placeholder="Rechercher un pokémon">
-        <label for="type" class="text-white">Filtrer par type:</label>
-        <select name="type" id="type" v-model="typeSelect">
-          <option value="all" selected>ALL</option>
-          <option v-for="pokeType in types" :value="pokeType.nom" :key="pokeType.nom">{{
-            pokeType.nom }}</option>
-        </select>
+    <div class="bg-red-500 w-1/2 p-5 rounded-l-lg h-[82vh]">
+      <div class="h-[100%]">
+        <div class="py-5 px-3 space-x-4 bg-zinc-600 rounded-t-lg shadow-xl">
+          <input type="text" v-model="searchTxt" placeholder="Rechercher un pokémon">
+          <label for="type" class="text-white">Filtrer par type:</label>
+          <select name="type" id="type" v-model="typeSelect">
+            <option value="all" selected>ALL</option>
+            <option v-for="pokeType in types" :value="pokeType.nom" :key="pokeType.nom">{{
+              pokeType.nom }}</option>
+          </select>
+        </div>
+        <ul v-if="pokemonsFiltered.length || !searchTxt" class="block overflow-scroll bg-zinc-400 p-2 rounded-b-lg"
+          style="height: calc(100% - 64px);">
+          <li v-for="pokemon in pokemonsFiltered.length ? pokemonsFiltered : pokemons" :key="pokemon.id">
+            <button @click="selectPokemon(pokemon.slug)" :class="[
+              'group', 'w-full', 'aspect-w-1/1', 'border-2', 'flex', 'items-center', 'my-5', 'hover:bg-zinc-300', 'hover:text-blue-500', 'rounded-full', 'transition',
+              pokemon.nom === selectedPokemon?.nom ? ['bg-blue-500', 'underline', 'text-white'] : 'bg-gray-500'
+            ]">
+              <NuxtImg :src="pokemon.image.url" :alt="pokemon.nom"
+                class="inline-block h-12 w-12 self-center bg-blue-200 rounded-l-full" />
+              <h2 class="text-3xl text-left ml-2 text-white">{{ pokemon.nom }}</h2>
+            </button>
+          </li>
+        </ul>
+        <ul v-else>
+          <li>Loading...</li>
+        </ul>
       </div>
-      <ul v-if="pokemonsFiltered.length || !searchTxt" class="h-[70vh] overflow-scroll bg-zinc-400 p-2 rounded-b-lg">
-        <li v-for="pokemon in pokemonsFiltered.length ? pokemonsFiltered : pokemons" :key="pokemon.id">
-          <button @click="selectPokemon(pokemon.slug)" :class="[
-            'group', 'w-full', 'aspect-w-1/1', 'border-2', 'flex', 'items-center', 'my-5', 'hover:bg-zinc-300', 'hover:text-blue-500', 'rounded-full', 'transition',
-            pokemon.nom === selectedPokemon?.nom ? ['bg-blue-500', 'underline', 'text-white'] : 'bg-gray-500'
-          ]">
-            <NuxtImg :src="pokemon.image.url" :alt="pokemon.nom"
-              class="inline-block h-12 w-12 self-center bg-blue-200 rounded-l-full" />
-            <h2 class="text-3xl text-left ml-2">{{ pokemon.nom }}</h2>
-          </button>
-        </li>
-      </ul>
-      <ul v-else>
-        <li>Loading...</li>
-      </ul>
     </div>
-    <div v-if="selectedPokemon" class="bg-blue-500 w-1/2 p-5 rounded-r-lg h-[82vh]">
+    <div v-if="selectedPokemon" :style="{ backgroundColor: selectedPokemon?.couleur.hex }"
+      class="w-1/2 p-5 rounded-r-lg h-[82vh]">
       <div class="bg-white rounded-lg h-full p-5 space-y-4 overflow-scroll">
         <NuxtImg class="h-80 m-auto rounded-full" :src="selectedPokemon?.image.url" :alt="selectedPokemon?.nom" />
         <h2 class="text-3xl text-center font-bold">{{ selectedPokemon?.nom }}</h2>
@@ -127,13 +131,16 @@ const pokemonsFiltered = computed(() => {
         <hr class="border-black border-1 my-3">
         <ul class="grid grid-cols-2 space-y-4 place-items-center">
           <li class="col-span-2"><span class="font-bold">Couleur:</span>
-            <div :style="{ backgroundColor: selectedPokemon?.pokemonType?.couleur.hex }"
+            <div :style="{ backgroundColor: selectedPokemon?.couleur.hex }"
               class="w-6 h-6 inline-block rounded-full ml-2">
             </div>
             {{
-              selectedPokemon?.pokemonType?.couleur.hex }}
+              selectedPokemon?.couleur.hex }}
           </li>
-          <li><span class="font-bold">Type:</span> {{ selectedPokemon?.pokemonType?.nom }}</li>
+          <li><span class="font-bold">Type:</span><img class="h-10 inline-block ml-2 rounded-full"
+              :src="selectedPokemon?.pokemonType?.image.url" :alt="selectedPokemon?.pokemonType?.nom"> {{
+                selectedPokemon?.pokemonType?.nom }}
+          </li>
           <li><span class="font-bold">PV:</span> {{ selectedPokemon?.pv }}</li>
           <li><span class="font-bold">Taille:</span> {{ selectedPokemon?.taille }} m</li>
           <li><span class="font-bold">Poids:</span> {{ selectedPokemon?.poids }} kg</li>
